8eb945af39abaa87e6e267f819b3702bd42ba735
chetan-pachpande <chetandpachpande@gmail.com>
2025-06-29 11:09:45 -0700

    Remove all C# and Python files, keep only Java - clean Java-only repository

--- 01_Arrays_Hashing/Problems/LC_049_GroupAnagrams/groupanagrams.py
+++
@@ -1,6 +0,0 @@
-def solution():
-    # TODO: Implement your solution here
-    print("Groupanagrams solution in Python")
-
-if __name__ == "__main__":
-    solution()
--- 01_Arrays_Hashing/Problems/LC_242_ValidAnagram/validanagram.py
+++
@@ -1,6 +0,0 @@
-def solution():
-    # TODO: Implement your solution here
-    print("Validanagram solution in Python")
-
-if __name__ == "__main__":
-    solution()
